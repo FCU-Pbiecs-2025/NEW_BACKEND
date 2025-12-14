128b7c054d5bd01039635a28a8d61f709481649f
CHEN,CHUN-AN <D1397030@o365.fcu.edu.tw>
2025-12-14 11:07:58 +0800

    20251214 1.補上傳

---
+++ Childcare/src/main/java/Group4/Childcare/DTO/LoginRequest.java
@@ -0,0 +1,33 @@
+package Group4.Childcare.DTO;
+
+public class LoginRequest {
+    private String account;
+    private String password;
+    private String recaptchaToken;
+
+    // Getters and setters
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRecaptchaToken() {
+        return recaptchaToken;
+    }
+
+    public void setRecaptchaToken(String recaptchaToken) {
+        this.recaptchaToken = recaptchaToken;
+    }
+}
+
---
+++ Childcare/src/main/java/Group4/Childcare/DTO/RecaptchaResponse.java
@@ -0,0 +1,54 @@
+package Group4.Childcare.DTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class RecaptchaResponse {
+    private boolean success;
+    private double score;
+    private String action;
+    @JsonProperty("challenge_ts")
+    private String challengeTs;
+    private String hostname;
+
+    // Getters and setters
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getChallengeTs() {
+        return challengeTs;
+    }
+
+    public void setChallengeTs(String challengeTs) {
+        this.challengeTs = challengeTs;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+}
+
